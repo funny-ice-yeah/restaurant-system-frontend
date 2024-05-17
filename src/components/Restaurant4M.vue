1a8eb5d11325fa9e48cae8fb19b6a268f1f375d8
WangYang <2319008062@qq.com>
2024-05-17 19:12:17 +0800

    完成管理员界面

--- src/components/Restaurant4M.vue
+++ src/components/Restaurant4M.vue
@@ -4,17 +4,20 @@
             <el-table-column fixed="left" prop="restaurantId" label="Id" width="180" />
             <el-table-column prop="restaurantName" label="店名" width="180" />
             <el-table-column prop="location" label="地址" width="180" />
-            <el-table-column prop="account" label="账号" width="180" /> 
+            <el-table-column prop="account" label="账号" width="180" />
             <el-table-column prop="password" label="密码" width="180" />
             <el-table-column prop="canteenId" label="食堂Id" width="180" />
-            <el-table-column prop="briefIntro" label="简介" width="180" /> 
-            <el-table-column fixed="right" label="操作" width="120">
-                <template #default>
+            <el-table-column prop="briefIntro" label="简介" width="180" />
+            <el-table-column fixed="right" label="操作" width="180">
+                <template #default="scope">
                     <el-button link type="primary" size="small">
                         详情
                     </el-button>
-                    <el-button link type="primary" size="small">
+                    <el-button link type="primary" size="small" @click="updateClick(scope.row)">
                         编辑
+                    </el-button>
+                    <el-button link type="primary" size="small" @click="deleteRestaurant(scope.row)">
+                        删除
                     </el-button>
                 </template>
             </el-table-column>
@@ -23,24 +26,132 @@
     <!-- <div>
         <el-pagination background layout="prev, pager, next" :total="total" :page-size="page_size"/>
     </div> -->
+    <el-button plain @click="addVisible = true">
+        新增商家
+    </el-button>
+    <el-dialog v-model="addVisible">
+        <el-form :model="newRestaurant" label-width="auto" style="max-width: 600px">
+            <el-form-item label="店名" placeholder="">
+                <el-input v-model="newRestaurant.restaurantName" />
+            </el-form-item>
+            <el-form-item label="地址" placeholder="">
+                <el-input v-model="newRestaurant.location" />
+            </el-form-item>
+            <el-form-item label="账号" placeholder="">
+                <el-input v-model="newRestaurant.account" />
+            </el-form-item>
+            <el-form-item label="密码" placeholder="">
+                <el-input v-model="newRestaurant.password" />
+            </el-form-item>
+            <el-form-item label="食堂Id" placeholder="">
+                <el-input v-model="newRestaurant.canteenId" />
+            </el-form-item>
+            <el-form-item label="简介" placeholder="">
+                <el-input v-model="newRestaurant.briefIntro" />
+            </el-form-item>
+        </el-form>
+        <template #footer>
+            <div class="dialog-footer">
+                <el-button type="primary" @click="addVisible = false">
+                    取消
+                </el-button>
+                <el-button type="primary" @click="addRestaurant">
+                    确定
+                </el-button>
+            </div>
+        </template>
+    </el-dialog>
+    <el-dialog v-model="updateVisible">
+        <el-form :model="updateRestaurant" label-width="auto" style="max-width: 600px">
+            <el-form-item label="店名" placeholder="">
+                <el-input v-model="updateRestaurant.restaurantName" />
+            </el-form-item>
+            <el-form-item label="地址" placeholder="">
+                <el-input v-model="updateRestaurant.location" />
+            </el-form-item>
+            <el-form-item label="账号" placeholder="">
+                <el-input v-model="updateRestaurant.account" />
+            </el-form-item>
+            <el-form-item label="密码" placeholder="">
+                <el-input v-model="updateRestaurant.password" />
+            </el-form-item>
+            <el-form-item label="食堂Id" placeholder="">
+                <el-input v-model="updateRestaurant.canteenId" />
+            </el-form-item>
+            <el-form-item label="简介" placeholder="">
+                <el-input v-model="updateRestaurant.briefIntro" />
+            </el-form-item>
+        </el-form>
+        <template #footer>
+            <div class="dialog-footer">
+                <el-button type="primary" @click="updateVisible = false">
+                    取消
+                </el-button>
+                <el-button type="primary" @click="updateConfirm">
+                    确定
+                </el-button>
+            </div>
+        </template>
+    </el-dialog>
 </template>
 
 <script setup>
-import {ref} from 'vue'
+import { ref } from 'vue'
 import { useRouter, useRoute } from 'vue-router'
 import axios from 'axios'
 
 const restaurants = ref([])
 const total = ref(100)
 const page_size = ref(10)
-
-axios.get("http://localhost:8080/restaurant/selectAll", {
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      withCredentials: true 
+const addVisible = ref(false)
+const newRestaurant = ref({ restaurantName: null, lcation: null, account: null, password: null, canteenId: null, briefIntro: null })
+const updateRestaurant = ref({ restaurantId: null, restaurantName: null, lcation: null, account: null, password: null, canteenId: null, briefIntro: null })
+const updateVisible = ref(false)
+const addRestaurant = () => {
+    axios.post("http://localhost:8080/restaurant", newRestaurant.value, {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        withCredentials: true
+    }).then((response) => {
+        getRestaurant()
+        addVisible.value = false
+        newRestaurant.value = { restaurantName: null, lcation: null, account: null, password: null, canteenId: null, briefIntro: null }
+    })
+}
+const updateClick = (row) => {
+    updateVisible.value = true
+    updateRestaurant.value = row
+}
+const updateConfirm = () => {
+    axios.put("http://localhost:8080/restaurant", updateRestaurant.value, {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        withCredentials: true 
+    }).then((response) => {
+        getRestaurant()
+        updateVisible.value = false
+        updateRestaurant.value = { restaurantId: null, restaurantName: null, lcation: null, account: null, password: null, canteenId: null, briefIntro: null }
+    })
+}
+const deleteRestaurant = (row) => {
+    axios.delete("http://localhost:8080/restaurant/deleteById", {
+        withCredentials: true,
+        params: {
+            restaurantId: row.restaurantId
+        }
+    }).then((response) => {
+        getRestaurant()
+    })
+}
+const getRestaurant = () => {
+    axios.get("http://localhost:8080/restaurant/selectAll", {
+        withCredentials: true
     }).then((response) => {
         restaurants.value = response.data
         total.value = response.data.length
-    }) 
+    })
+}
+getRestaurant()
 </script>
--- src/components/User4M.vue
+++ src/components/User4M.vue
@@ -9,12 +9,12 @@
             <el-table-column prop="role" label="角色" width="180" />
             <el-table-column prop="roleId" label="学号/工号" width="180" />
             <el-table-column fixed="right" label="操作" width="120">
-                <template #default>
-                    <el-button link type="primary" size="small">
-                        详情
-                    </el-button>
-                    <el-button link type="primary" size="small">
+                <template #default="scope">
+                    <el-button link type="primary" size="small" @click="updateUserClick(scope.row)">
                         编辑
+                    </el-button>
+                    <el-button link type="primary" size="small" @click="deleteUser(scope.row)">
+                        删除
                     </el-button>
                 </template>
             </el-table-column>
@@ -23,6 +23,73 @@
     <!-- <div>
         <el-pagination background layout="prev, pager, next" :total="total" :page-size="page_size" />
     </div> -->
+    <el-button plain @click="addVisible=true">
+        新增用户
+    </el-button>
+    <el-dialog v-model="addVisible">
+        <el-form :model="newUser" label-width="auto" style="max-width: 600px">
+            <el-form-item label="姓名" placeholder="">
+                <el-input v-model="newUser.userName" />
+            </el-form-item>
+            <el-form-item label="性别" placeholder="">
+                <el-input v-model="newUser.gender" />
+            </el-form-item>
+            <el-form-item label="密码" placeholder="">
+                <el-input v-model="newUser.password" />
+            </el-form-item>
+            <el-form-item label="年龄" placeholder="">
+                <el-input v-model="newUser.age" />
+            </el-form-item>
+            <el-form-item label="身份" placeholder="">
+                <el-input v-model="newUser.role" />
+            </el-form-item>
+            <el-form-item label="学号/工号" placeholder="">
+                <el-input v-model="newUser.roleId" />
+            </el-form-item>
+        </el-form>
+        <template #footer>
+            <div class="dialog-footer">
+                <el-button type="primary" @click="addVisible=false">
+                    取消
+                </el-button>
+                <el-button type="primary" @click="addUser">
+                    确定
+                </el-button>
+            </div>
+        </template>
+    </el-dialog>
+    <el-dialog v-model="updateVisible">
+        <el-form :model="updateUser" label-width="auto" style="max-width: 600px"> 
+            <el-form-item label="姓名" placeholder="">
+            <el-input v-model="updateUser.userName" />
+            </el-form-item>
+            <el-form-item label="性别" placeholder="">
+                <el-input v-model="updateUser.gender" />
+            </el-form-item>
+            <el-form-item label="密码" placeholder="">
+                <el-input v-model="updateUser.password" />
+            </el-form-item>
+            <el-form-item label="年龄" placeholder="">
+                <el-input v-model="updateUser.age" />
+            </el-form-item>
+            <el-form-item label="身份" placeholder="">
+                <el-input v-model="updateUser.role" />
+            </el-form-item>
+            <el-form-item label="学号/工号" placeholder="">
+                <el-input v-model="updateUser.roleId" />
+            </el-form-item>
+        </el-form>
+        <template #footer>
+            <div class="dialog-footer">
+                <el-button type="primary" @click="updateVisible=false">
+                    取消
+                </el-button>
+                <el-button type="primary" @click="updateUserConfirm">
+                    确定
+                </el-button>
+            </div>
+        </template>
+    </el-dialog>
 </template>
 
 <script setup>
@@ -32,13 +99,63 @@ import axios from 'axios'
 const users = ref([])
 const total = ref(100)
 const page_size = ref(10)
-axios.get("http://localhost:8080/user/selectAll", {
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      withCredentials: true 
+const newUser = ref({ userName: null, age: null, password: null, age: null, roleId: null, role: null })
+const updateUser = ref({userId: null, userName: null, age: null, password: null, age: null, roleId: null, role: null }) 
+const addVisible = ref(false)
+const updateVisible = ref(false)
+const addUser = () => {
+    formVisible.value = false
+    axios.post("http://localhost:8080/user", user.value, {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        withCredentials: true
+    }).then((response) => {
+        getUser()
+        newUser.value = { userName: null, age: null, password: null, age: null, roleId: null, role: null }
+    })
+}
+const deleteUser = (row) => {
+    axios.delete("http://localhost:8080/user/deleteById", {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            withCredentials: true,
+            params: {
+                userId: row.userId
+            }
+        }).then((response) => {
+            getUser()
+        })
+}
+const getUser = () => {
+    axios.get("http://localhost:8080/user/selectAll", {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        withCredentials: true
     }).then((response) => {
         users.value = response.data
         total.value = response.data.length
-    }) 
+    })
+}
+const updateUserClick = (row) => {
+    updateUser.value = row
+    updateVisible.value = true
+}
+const updateUserConfirm = () => {
+    console.log(updateUser.value)
+    axios.put("http://localhost:8080/user", updateUser.value, {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        withCredentials: true
+    }).then((response) => {
+        updateVisible.value = false
+        getUser()
+        updateRestaurant.value = {userId: null, userName: null, age: null, password: null, age: null, roleId: null, role: null }
+    })
+}
+getUser()
+
 </script>
