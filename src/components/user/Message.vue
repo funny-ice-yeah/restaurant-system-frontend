1e08cf06a23060cdd92018477cd6fce15672fd15
WangYang <2319008062@qq.com>
2024-06-04 19:16:37 +0800

    debug: 修复了Message页面的错误

--- src/components/user/Message.vue
+++ src/components/user/Message.vue
@@ -1,17 +1,10 @@
 <template>
     <div>
         <el-table :data="messages" style="width: 100%">
-            <el-table-column fixed="left" prop="message_id" label="Id" width="180" />
+            <el-table-column fixed="left" prop="messageId" label="Id" width="180" />
             <el-table-column prop="subject" label="主题" width="180" />
             <el-table-column prop="content" label="内容" width="180" />
-            <el-table-column prop="order_id" label="订单Id" width="180" />
-            <el-table-column fixed="right" label="操作" width="120">
-                <template #default>
-                    <el-button link type="primary" size="small">
-                        详情
-                    </el-button>
-                </template>
-            </el-table-column>
+            <el-table-column prop="orderId" label="订单Id" width="180" />
         </el-table>
     </div>
 </template>
