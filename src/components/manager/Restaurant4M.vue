f5b1bdf5bea799f6df2c58aae2420f3b9f7b28f1
DiaoZiJie <22307130433@m.fudan.edu.cn>
2024-06-20 23:08:33 +0800

    feature:将食堂Id映射为食堂名

--- src/components/manager/Restaurant4M.vue
+++ src/components/manager/Restaurant4M.vue
@@ -6,7 +6,11 @@
             <el-table-column prop="location" label="地址" width="180" />
             <el-table-column prop="account" label="账号" width="180" />
             <el-table-column prop="password" label="密码" width="180" />
-            <el-table-column prop="canteenId" label="食堂Id" width="180" />
+            <el-table-column label="食堂名称" width="180" >
+                <template #default="scope">
+                    {{ canteenMap[scope.row.canteenId] }}
+                </template>
+            </el-table-column>
             <el-table-column prop="briefIntro" label="简介" width="180" />
             <el-table-column fixed="right" label="操作" width="180">
                 <template #default="scope">
@@ -34,17 +38,24 @@
             <el-form-item label="店名" placeholder="">
                 <el-input v-model="newRestaurant.restaurantName" />
             </el-form-item>
-            <el-form-item label="地址" placeholder="">
-                <el-input v-model="newRestaurant.location" />
-            </el-form-item>
             <el-form-item label="账号" placeholder="">
                 <el-input v-model="newRestaurant.account" />
             </el-form-item>
             <el-form-item label="密码" placeholder="">
                 <el-input v-model="newRestaurant.password" />
             </el-form-item>
-            <el-form-item label="食堂Id" placeholder="">
-                <el-input v-model="newRestaurant.canteenId" />
+            <el-form-item label="食堂名称" placeholder="">
+                <el-select v-model="newRestaurant.canteenId" placeholder="">
+                    <el-option
+                        v-for="(name, id) in canteenMap"
+                        :key="id"
+                        :label="name"
+                        :value="id"
+                    />
+                </el-select>
+            </el-form-item>
+            <el-form-item label="相对位置" placeholder="">
+                <el-input v-model="newRestaurant.location" />
             </el-form-item>
             <el-form-item label="简介" placeholder="">
                 <el-input v-model="newRestaurant.briefIntro" />
@@ -66,17 +77,24 @@
             <el-form-item label="店名" placeholder="">
                 <el-input v-model="updateRestaurant.restaurantName" />
             </el-form-item>
-            <el-form-item label="地址" placeholder="">
-                <el-input v-model="updateRestaurant.location" />
-            </el-form-item>
             <el-form-item label="账号" placeholder="">
                 <el-input v-model="updateRestaurant.account" />
             </el-form-item>
             <el-form-item label="密码" placeholder="">
                 <el-input v-model="updateRestaurant.password" />
             </el-form-item>
-            <el-form-item label="食堂Id" placeholder="">
-                <el-input v-model="updateRestaurant.canteenId" />
+            <el-form-item label="食堂名称" placeholder="">
+                <el-select v-model="updateRestaurant.canteenId" placeholder="请选择">
+                    <el-option
+                        v-for="(name, id) in canteenMap"
+                        :key="id"
+                        :label="name"
+                        :value="id"
+                    />
+                </el-select>
+            </el-form-item>
+            <el-form-item label="相对位置" placeholder="">
+                <el-input v-model="updateRestaurant.location" />
             </el-form-item>
             <el-form-item label="简介" placeholder="">
                 <el-input v-model="updateRestaurant.briefIntro" />
@@ -99,6 +117,7 @@
 import { ref } from 'vue'
 import { useRouter, useRoute } from 'vue-router'
 import axios from 'axios'
+import { ElMessage } from 'element-plus'
 
 const restaurants = ref([])
 const total = ref(100)
@@ -107,6 +126,15 @@ const addVisible = ref(false)
 const newRestaurant = ref({ restaurantName: null, lcation: null, account: null, password: null, canteenId: null, briefIntro: null })
 const updateRestaurant = ref({ restaurantId: null, restaurantName: null, lcation: null, account: null, password: null, canteenId: null, briefIntro: null })
 const updateVisible = ref(false)
+// 食堂ID到名称的映射
+const canteenMap = {
+    1: '旦苑餐厅',
+    2: '南苑餐厅',
+    3: '北区食堂',
+    4: '江湾食堂',
+    5: '枫林西餐厅',
+    6: '张江食堂'
+}
 const addRestaurant = () => {
     axios.post("http://localhost:8080/restaurant", newRestaurant.value, {
         headers: {
@@ -121,9 +149,17 @@ const addRestaurant = () => {
 }
 const updateClick = (row) => {
     updateVisible.value = true
-    updateRestaurant.value = {...row}
+    updateRestaurant.value = {...row,location:null,canteenId:null}
 }
 const updateConfirm = () => {
+    if (!updateRestaurant.value.canteenId) {
+        ElMessage.error("请选择食堂")
+        return;
+    }
+    if (!updateRestaurant.value.location){
+        ElMessage.error("请输入新相对地址")
+        return;        
+    }    
     axios.put("http://localhost:8080/restaurant", updateRestaurant.value, {
         headers: {
             'Content-Type': 'application/json'
